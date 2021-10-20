2780fe7406a8f77aa58f0d4f07847b767c6a0f6d
ViliKo <59921526+ViliKo@users.noreply.github.com>
2021-10-20 20:42:25 +0300

    Finished task

--- src/index.js
+++ src/index.js
@@ -7,21 +7,23 @@ let textString = "";
 let customStyle;
 
 if (dateTimeFound >= 5 && dateTimeFound < 12) {
-  textString = "good Morning";
+  textString = "Good Morning";
+  customStyle = { color: "red" };
+} else if (dateTimeFound >= 12 && dateTimeFound < 18) {
+  textString = "Good Afternoon";
   customStyle = { color: "green" };
-} else if (dateTimeFound >= 12 && dateTimeFound < 17) {
-  textString = "good afternoon";
-  customStyle = { color: "blue" };
 } else if (
-  (dateTimeFound >= 17 && dateTimeFound < 23) ||
+  (dateTimeFound >= 18 && dateTimeFound < 23) ||
   (dateTimeFound >= 0 && dateTimeFound < 5)
 ) {
-  textString = "good evening";
+  textString = "Good Evening";
   customStyle = { color: "blue" };
 }
 
 ReactDom.render(
-  <h1 style={customStyle}>{textString}</h1>,
+  <h1 className="heading" style={customStyle}>
+    {textString}
+  </h1>,
   document.getElementById("root")
 );
 
